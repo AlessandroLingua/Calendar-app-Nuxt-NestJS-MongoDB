f18c1eeca9f5f402f96fec490e3483485614e06b
Alessandro_Lingua <kanga.coding93@gmail.com>
2024-06-28 19:21:59 +0200

    update

--- frontend/src/api.service.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const API_URL = 'http://localhost:3000';
-
-export const getEvents = () => axios.get(`${API_URL}/events`);
-
-export const createEvent = (eventData) => axios.post(`${API_URL}/events`, eventData);
